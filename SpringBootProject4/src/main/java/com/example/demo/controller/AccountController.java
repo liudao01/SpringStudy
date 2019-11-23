0b353a71f0272a83f94cd34088a6e8fe2496c486
Liuml <951442843@qq.com>
2019-11-23 12:14:57 +0800

    显示图片

--- SpringBootProject4/src/main/java/com/example/demo/controller/AccountController.java
+++ SpringBootProject4/src/main/java/com/example/demo/controller/AccountController.java
@@ -22,189 +22,195 @@ import com.github.pagehelper.PageInfo;
 
 /**
  * 用户账号相关操作
- * 
- * @author liumaolin
  *
+ * @author liumaolin
  */
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
-	@Autowired
-	AccountService accountService;
-	
-	@Autowired
-	Config config;
+    @Autowired
+    AccountService accountService;
 
-	/**
-	 * 登录
-	 * 
-	 * @return
-	 */
-	@RequestMapping("login")
-	public String login(Model model) {
-		model.addAttribute("config",config);
-		return "account/login";
-	}
+    @Autowired
+    Config config;
 
-	/**
-	 * 注册
-	 * 
-	 * @return
-	 */
-	@RequestMapping("register")
-	public String register() {
-		return "account/register";
-	}
+    /**
+     * 登录
+     *
+     * @return
+     */
+    @RequestMapping("login")
+    public String login(Model model) {
+        model.addAttribute("config", config);
+        return "account/login";
+    }
 
-	/**
-	 * 注册用户
-	 * 
-	 * @param loginName
-	 * @param password
-	 * @param nickName
-	 * @param age
-	 * @param location
-	 * @return
-	 */
-	@RequestMapping("/registerUser")
-	@ResponseBody
-	public RespStat registerUser(String loginName, String password, String nickName, Integer age, String location) {
+    /**
+     * 注册
+     *
+     * @return
+     */
+    @RequestMapping("register")
+    public String register() {
+        return "account/register";
+    }
 
-		System.out.println("注册用户 loginName " + loginName);
-		// 标记是否删除成功 status
-		RespStat respStat = accountService.reguster(loginName, password, nickName, age, location);
-		return respStat;
-	}
+    /**
+     * 注册用户
+     *
+     * @param loginName
+     * @param password
+     * @param nickName
+     * @param age
+     * @param location
+     * @return
+     */
+    @RequestMapping("/registerUser")
+    @ResponseBody
+    public RespStat registerUser(String loginName, String password, String nickName, Integer age, String location) {
 
-	/**
-	 * 校验账户 数据校验
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/validataAccount")
-	@ResponseBody
-	public String validataAccount(String loginName, String password, HttpServletRequest request) {
-		System.out.print("loginname=" + loginName);
+        System.out.println("注册用户 loginName " + loginName);
+        // 标记是否删除成功 status
+        RespStat respStat = accountService.reguster(loginName, password, nickName, age, location);
+        return respStat;
+    }
 
-		System.out.print("password=" + password);
-		// 1 直接返回是否登录成功的结果
-		// 2. 返回Account 对象 若对象是空的 在controller 里做业务逻辑处理
 
-		Account account = accountService.findByLoginAndPassword(loginName, password);
+    /**
+     * 校验账户 数据校验
+     *
+     * @return
+     */
+    @RequestMapping("/validataAccount")
+    @ResponseBody
+    public String validataAccount(String loginName, String password, HttpServletRequest request) {
+        System.out.print("loginname=" + loginName);
 
-		if (account == null) {
-			return "登录失败";
-		} else {
-			// 登录成功
-			// 让service 返回对象 如果登录成功把用户对象 写到session里
-			// 在不同controller 或者前端页面上都能使用当前Account对象
-			request.getSession().setAttribute("account", account);
-			return "success";
-		}
+        System.out.print("password=" + password);
+        // 1 直接返回是否登录成功的结果
+        // 2. 返回Account 对象 若对象是空的 在controller 里做业务逻辑处理
 
-	}
+        Account account = accountService.findByLoginAndPassword(loginName, password);
 
-	/**
-	 * 登出
-	 * 
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping("/logOut")
-	public String logOut(HttpServletRequest request) {
+        if (account == null) {
+            return "登录失败";
+        } else {
+            // 登录成功
+            // 让service 返回对象 如果登录成功把用户对象 写到session里
+            // 在不同controller 或者前端页面上都能使用当前Account对象
+            request.getSession().setAttribute("account", account);
+            return "success";
+        }
 
-		request.getSession().removeAttribute("account");
-		return "index";
-	}
+    }
 
-	/**
-	 * 用户列表
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/list")
-	public String list(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "5") int pageSize,
-			Model model) {
+    /**
+     * 登出
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("/logOut")
+    public String logOut(HttpServletRequest request) {
 
-		PageInfo<Account> page = accountService.findByPage(pageNum, pageSize);
+        request.getSession().removeAttribute("account");
+        return "index";
+    }
 
-		model.addAttribute("page", page);
-		return "account/list";
-	}
+    /**
+     * 用户列表
+     *
+     * @return
+     */
+    @RequestMapping("/list")
+    public String list(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "5") int pageSize,
+                       Model model) {
 
-	/**
-	 * 根据id 删除用户 TODO 需要校验是否当前登录用户 如果是当前登录用户 不能删除
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/deleteById")
-	@ResponseBody
-	public RespStat deleteById(int id) {
-		// 标记是否删除成功 status
-		RespStat respStat = accountService.deleteById(id);
-		return respStat;
-	}
+        PageInfo<Account> page = accountService.findByPage(pageNum, pageSize);
 
-	@RequestMapping("/profile")
-	public String profile() {
+        model.addAttribute("page", page);
+        return "account/list";
+    }
 
-		try {
-			File path = new File(ResourceUtils.getURL("classpath:").getPath());
-			File upload = new File(path.getAbsolutePath(), "static/upload/");
-			System.out.println(upload.getAbsolutePath());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		;
-		return "account/profile";
-	}
+    /**
+     * 根据id 删除用户 TODO 需要校验是否当前登录用户 如果是当前登录用户 不能删除
+     *
+     * @return
+     */
+    @RequestMapping("/deleteById")
+    @ResponseBody
+    public RespStat deleteById(int id) {
+        // 标记是否删除成功 status
+        RespStat respStat = accountService.deleteById(id);
+        return respStat;
+    }
 
-	/**
-	 * 文件上传 这是在jar包解开的情况下才可以 如果打成jar包 不能在包里面写文件.
-	 * 
-	 * 应该做一次资源映射
-	 * 
-	 * @param filename
-	 * @param password
-	 * @return
-	 */
-	@RequestMapping("/fileUploadController")
-	@ResponseBody
-	public String fileUpload(MultipartFile filename, String password,HttpServletRequest httpServletRequest) {
-		System.out.println("password:" + password);
-		System.out.println("file:" + filename.getOriginalFilename());
-		Account account = (Account)httpServletRequest.getSession().getAttribute("account");
+    @RequestMapping("/profile")
+    public String profile() {
 
-		try {
+        try {
+            File path = new File(ResourceUtils.getURL("classpath:").getPath());
+            File upload = new File(path.getAbsolutePath(), "static/upload/");
+            System.out.println(upload.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        ;
+        return "account/profile";
+    }
+
+    /**
+     * 文件上传 这是在jar包解开的情况下才可以 如果打成jar包 不能在包里面写文件.
+     * <p>
+     * 应该做一次资源映射
+     *
+     * @param filename
+     * @param password
+     * @return
+     */
+    @RequestMapping("/fileUploadController")
+    @ResponseBody
+    public String fileUpload(MultipartFile filename, String password, HttpServletRequest httpServletRequest) {
+        System.out.println("password:" + password);
+        System.out.println("file:" + filename.getOriginalFilename());
+        Account account = (Account)httpServletRequest.getSession().getAttribute("account");
+
+		String path = "d:/testUpLoad";
+//		String path = "/Users/liumaolin/Downloads/project_study";
+        try {
+//            File path = new File(ResourceUtils.getURL("classpath:").getPath());
+//            File upload = new File(path.getAbsolutePath(), "static/upload/");
 //			accountService.update
-			//原始的位置  映射的   ResourceUtils.getURL("classpath:").getPath()
-			System.out.println("ResourceUtils.getURL(\"classpath:\").getPath() = "
-					+ ResourceUtils.getURL("classpath:").getPath());
-			///Users/liumaolin/Downloads/project_study
-			//暂时写死 本地的路径  需要加上 static/upload/ 
+            //原始的位置  映射的   ResourceUtils.getURL("classpath:").getPath()
+            System.out.println("ResourceUtils.getURL(\"classpath:\").getPath() = "
+                + ResourceUtils.getURL("classpath:").getPath());
+            ///Users/liumaolin/Downloads/project_study
+            //暂时写死 本地的路径  需要加上 static/upload/
 //			File path = new File(ResourceUtils.getURL("classpath:").getPath());
 //			File upload = new File(path.getAbsolutePath(), "static/upload/");
 
-			System.out.println("filename.getOriginalFilename() = "+filename.getOriginalFilename());
+            System.out.println("filename.getOriginalFilename() = " + filename.getOriginalFilename());
 
-			filename.transferTo(new File("/Users/liumaolin/Downloads/project_study" + "/" + filename.getOriginalFilename()));
+            filename.transferTo(new File(path + "/" + filename.getOriginalFilename()));
+//            filename.transferTo(new File(path + "/" + filename.getOriginalFilename()));
 
-			account.setLocation(filename.getOriginalFilename());//设置头像
-			account.setPassword(password);//密码
 
-			accountService.update(account);
+            account.setLocation(filename.getOriginalFilename());//设置头像
+            account.setPassword(password);//密码
 
-			System.out.println("ok");
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "profile";
-	}
+            accountService.update(account);
+
+            System.out.println("ok");
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "profile";
+    }
 
 }

bb031fb0f87f74057d82c0a99aa3af23a431e11e
Liuml <951442843@qq.com>
2019-11-23 16:15:43 +0800

    角色权限管理页面ok

--- SpringBootProject4/src/main/java/com/example/demo/controller/ManagerController.java
+++ SpringBootProject4/src/main/java/com/example/demo/controller/ManagerController.java
@@ -1,12 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.demo.entity.Account;
 import com.example.demo.entity.Permission;
 import com.example.demo.entity.Role;
@@ -14,7 +7,14 @@ import com.example.demo.service.AccountService;
 import com.example.demo.service.PermissionService;
 import com.example.demo.service.RoleService;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 用户账号相关操作
@@ -91,13 +91,13 @@ public class ManagerController {
     }
 
     @RequestMapping("rolePermission/{id}")
-    @ResponseBody
-    public String rolePermission(@PathVariable int id, Model model) {
-        System.out.println("id = " + id);
+    public String rolePermission(@PathVariable("id") Integer id, Model model, HttpServletRequest request) {
+        System.out.println("id = " + "@PathVariable int id,"+id);
 //        PageInfo<Role> page = roleService.findByPage(pageNum, pageSize);
 //		PageInfo<Account> page = accountService.findByPage(pageNum, pageSize);
 //        model.addAttribute("page", page);
         return "manager/rolePermission";
     }
+
 
 }
